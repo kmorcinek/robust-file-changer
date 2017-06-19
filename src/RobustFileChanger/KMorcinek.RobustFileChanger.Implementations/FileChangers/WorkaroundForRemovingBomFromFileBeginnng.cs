7b2e538f4085b32b921ee4e96e5863ba2502586c
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-06-19 14:07:40 +0200

    Workaround

---
+++ src/RobustFileChanger/KMorcinek.RobustFileChanger.Implementations/FileChangers/WorkaroundForRemovingBomFromFileBeginnng.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using KMorcinek.RobustFileChanger.FileChangers;
+
+namespace KMorcinek.RobustFileChanger.Implementations.FileChangers
+{
+    public class WorkaroundForRemovingBomFromFileBeginnng : FileChangerBase
+    {
+        public override bool IsMatch(string[] lines)
+        {
+            return true;
+        }
+
+        public override IEnumerable<string> Transform(string[] lines)
+        {
+            return lines;
+        }
+
+        public override string SearchPattern => "*.ts";
+    }
+}
